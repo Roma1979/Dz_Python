ffb0f80613d56f85930c068f21298951386c0829
Roma <Romanote@mail.ru>
2022-10-08 23:03:14 +0300

    Задача 30

---
+++ dz_4_30.py
@@ -0,0 +1,13 @@
+# 30. Вычислить число pi c заданной точностью d
+
+import math
+from math import pi
+
+n = pi
+print(n)
+
+n = 100
+my_pi = sum(1/16**x*(4/(8*x + 1) - 2/(8*x + 4) - 1/(8*x + 5) - 1/(8*x + 6)) for x in range(n))
+
+print(my_pi)
+
